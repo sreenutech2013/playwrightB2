9689fad8cd1201241c4f184706409692dbc2a049
trinadhdn <trinadhdn@gmail.com>
2024-07-04 09:08:03 +0530

    MultiDropDown and suggestion dropdown handler

--- src/Pages/util.ts
+++ src/Pages/util.ts
@@ -1,19 +1,42 @@
 import { Locator, Page } from "@playwright/test"
 
-export class utils {
+export class UIhelper {
 
-    static async dropDownHandler(jsCode, page) {
-        await page.evaluate(jsCode)
+    page: Page;
+
+    constructor(page: Page) {
+        this.page = page
+    }
+
+    async dropDownHandler(jsCode) {
+        await this.page.evaluate(jsCode)
 
     }
 
-    static async multiDropDownHandler(locator: any, listOfValue: string[]) {
-        for (let eachLocater of locator) {
-            let value = await eachLocater.textContent()
-            if (listOfValue.includes(value.trim())){
-                await eachLocater.click({force: true})
+    async handleAllKindsOfDropDown(locator: string, listOfValue: any) {
+        let newLocator = await this.page.$$(locator);
+        for (let eachLocater of newLocator) {
+            let value: any = await eachLocater.textContent()
+            if (listOfValue.includes(value.toString().trim())) {
+                await eachLocater.click({ force: true })
+                if (typeof (listOfValue) == 'string') {
+                    break
+                }
             }
         }
 
+
+    }
+
+    static getRandomData() {
+        let result = '';
+        const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+        const charactersLength = characters.length;
+        let counter = 0;
+        while (counter < length) {
+            result += characters.charAt(Math.floor(Math.random() * charactersLength));
+            counter += 1;
+        }
+        return result;
     }
 }
--- src/tests/multiDropDownValue.spec.ts
+++ src/tests/multiDropDownValue.spec.ts
@@ -1,20 +1,28 @@
 import { test, expect } from "@playwright/test"
-import { utils } from "../Pages/util";
+import { DropDownPage } from "../Pages/dropDownPage";
 test.use({ baseURL: 'https://demoqa.com' })
 
+let dropDownPage: DropDownPage
 
-test("handle MultiDrop value field", async ({page}) => {
-    
+
+test.only("handle MultiDrop value field", async ({ page }) => {
+
     await page.goto("/select-menu");
-    await page.locator('//b[text()="Multiselect drop down"]//../../div').click()
-    let locator = await page.$$('//div[contains(@id,"react-select")]')
-    let listOfValue: any = ['Green']
-    for (let eachLocater of locator) {
-        let newdata = await eachLocater.textContent();
-        if (listOfValue.includes(newdata)) {
-            await eachLocater.click({ force: true })
-        }
-    }
-    // utils.multiDropDownHandler(locator, listOfValue);
+    dropDownPage = new DropDownPage(page);
+    await dropDownPage.clickOnMenuDropDownField();
+    let listOfValue: any = ['Green', 'Black']
+    await dropDownPage.selectValuesForMultiselectdropdownField(listOfValue);
 
 })
+
+
+test("handle suggestion dropdown value field", async ({ page }) => {
+
+    await page.goto("https://www.google.com/");
+    dropDownPage = new DropDownPage(page);
+    await dropDownPage.searchInGoogle('india');
+    let listOfValue: any = 'results'
+    await dropDownPage.selectAutoSuggestionFormGoogleSearch(listOfValue);
+
+})
+
